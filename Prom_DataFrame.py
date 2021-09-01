12c1020cbf445eaf416b3401cc99a5623ac474bc
VaneVarC <89417693+VaneVarC@users.noreply.github.com>
2021-08-31 22:14:01 -0500

    DataFrame leído desde internet

--- Prom_DataFrame.py
+++ Prom_DataFrame.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("Materias (1).csv")
+df = pd.read_csv("https://raw.githubusercontent.com/VaneVarC/Ciencia-de-datos/448609d9020d35e57f954e3bcb23b0425f22ef98/Materias%20(1).csv?token=AVKGPXKFVFNETLY7IAOAECDBHAT2C") #Cargado desde internet
+#df = pd.read_csv("Materias (1).csv") //Cargado desde la computadora 
 print("La lista es: \n", df)
 suma = df["Calificaciones"].sum()
 contador = df["Calificaciones"].count()
